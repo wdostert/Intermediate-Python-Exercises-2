cf15ff4e04d7576b6b3f6f56172fd45a37529dff
wdostert <wdostert@live.com>
2023-09-10 21:48:27 -0400

    exercise_03 completed

---
+++ exercise_03/main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+random_nums = np.random.rand(10)
+
+mean = np.mean(random_nums)
+median = np.median(random_nums)
+standard_deviation = np.std(random_nums)
+
+print("Random Numbers:", random_nums, " Mean:", mean, " Median:", median, " Standard Deviation:", standard_deviation)
+
